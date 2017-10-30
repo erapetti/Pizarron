a84a995708f59b9f13e27295c4341c774471c612
erapetti <erapetti@yahoo.com>
2017-10-30 11:05:06 -0300

    Primeros avances

--- api/controllers/PizarronController.js
+++ api/controllers/PizarronController.js
@@ -8,6 +8,30 @@
 module.exports = {
 
 	index: function(req,res) {
-		res.view({title:"Pizarrón de Elección de Horas"});
+		var stock = Array();
+		stock[0] = { DependId:1001, PlanId:27, CicloId:2, GradoId:5, OrientacionId:0, OpcionId:36, MateriaNombre:'MATEMÁTICA I', Grupos:'3'};
+		stock[1] = { DependId:1001, PlanId:27, CicloId:2, GradoId:5, OrientacionId:0, OpcionId:36, MateriaNombre:'MATEMÁTICA III', Grupos:'2'};
+
+		Dependencias.find({StatusId:1}).exec(function(err,dependencias){
+			if (err) {
+				return res.serverError(err);
+			}
+			Planes.find({PlanActivo:1}).exec(function(err,planes){
+				if (err) {
+					return res.serverError(err);
+				}
+				Ciclos.find().exec(function(err,ciclos){
+					if (err) {
+						return res.serverError(err);
+					}
+					Grados.find().exec(function(err,grados){
+						if (err) {
+							return res.serverError(err);
+						}
+						res.view({title:"Pizarrón de Elección de Horas",dependencias:dependencias,planes:planes,ciclos:ciclos,grados:grados,stock:stock});
+					});
+				});
+			});
+		});
 	}
 };
---
+++ assets/js/pizarron.js
@@ -0,0 +1,17 @@
+function actualizaStock(){
+  var html;
+  for (var i = 0; i < stock.length; i++) {
+    html += "<tr><td>"+stock[i].DependId+
+            "</td><td>"+planes.find(function(e){return e.PlanId==stock[i].PlanId}).PlanAbrev+
+            "</td><td>"+ciclos.find(function(e){return e.CicloId==stock[i].CicloId}).CicloAbrev+
+            "</td><td>"+grados.find(function(e){return e.GradoId==stock[i].GradoId}).GradoAbrev+
+            "</td><td>"+stock[i].OrientacionId+
+            "</td><td>"+stock[i].OpcionId+
+            "</td><td>"+stock[i].MateriaNombre+
+            "</td><td>"+stock[i].Grupos+
+            "</td></tr>";
+  }
+  $('#stock > tbody').html(html);
+}
+
+$(document).ready(actualizaStock);
