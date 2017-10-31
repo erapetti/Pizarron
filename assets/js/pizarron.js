934e4498dd4602218b385a338b3922870dbf7a50
erapetti <erapetti@yahoo.com>
2017-10-31 09:33:36 -0300

    Funcionalidad de los dropdown

--- assets/js/pizarron.js
+++ assets/js/pizarron.js
@@ -1,3 +1,21 @@
+function actualizaDropDown(dd,nombres,clave,attrNombre){
+  // Busco los valores en uso en stock para la columna col
+  var seen = Array();
+  for (var i = 0; i < stock.length; i++) {
+    if (typeof stock[i][clave] !== 'undefined' && typeof seen[stock[i][clave]] === 'undefined') {
+      var item = nombres.find(function(e){return e[clave]==stock[i][clave]});
+      seen[stock[i][clave]] = item[attrNombre];
+    }
+  }
+  // Armo las opciones del dropdown
+  var html = "";
+  for (var key in seen) {
+    html += '<li><a href="#" dd="'+dd+'" data="'+key+'">'+seen[key]+'</a></li>';
+  }
+  // Actualizo el dropdown
+  $("ul.dropdown-menu[dd="+dd+"]").html( html );
+};
+
 function actualizaStock(){
   var html;
   for (var i = 0; i < stock.length; i++) {
@@ -12,6 +30,31 @@ function actualizaStock(){
             "</td></tr>";
   }
   $('#stock > tbody').html(html);
-}
-
+  //actualizaDropDown("dependencia",dependencias,"DependId","DependDesc");
+  actualizaDropDown("plan",planes,"PlanId","PlanAbrev");
+  actualizaDropDown("ciclo",ciclos,"CicloId","CicloAbrev");
+  actualizaDropDown("grado",grados,"GradoId","GradoAbrev");
+  //actualizaDropDown("materia",materias,"MateriaId","MateriaNombre");
+};
 $(document).ready(actualizaStock);
+
+$(document).ready(function() {
+  // inicialización de los dropdown-menu
+  $("ul.dropdown-menu").each(function(index,obj){
+    var campo = $(this).attr('dd');
+    var val = $('#dd-'+campo).val();
+    if (typeof val !== 'undefined' && val!=="") {
+      var texto = $("ul.dropdown-menu[dd="+campo+"] li a[data='"+val+"']").text();
+      // actualizo etiqueta del botón
+      $('#btn-dd-'+campo).html( texto + ' <span class="caret"></span>');
+    }
+  });
+  // función para actualizar los dropdown-menu cuando el usuario selecciona una opción
+  $("ul.dropdown-menu li a").click(function(event) {
+    event.preventDefault();
+    // actualizo input
+    $('#dd-'+$(this).attr('dd')).val( $(this).attr('data') );
+    // actualizo etiqueta del botón
+    $('#btn-dd-'+$(this).attr('dd')).html( $(this).text() + ' <span class="caret"></span>');
+  });
+});
