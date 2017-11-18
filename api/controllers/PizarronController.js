632fa6e1e3a779e156b3f771c03db79651bb8eca
erapetti <erapetti@yahoo.com>
2017-11-18 11:42:12 -0300

    Paso la consulta de stock por la cache

--- api/controllers/PizarronController.js
+++ api/controllers/PizarronController.js
@@ -21,65 +21,69 @@ String.prototype.hashCode = function() {
 // Función que se agrega a todos los modelos para hacer find con parámetro objQuery
 // buscando primero en memcached
 sails.config.models.cacheFind = function (objQuery,callback) {
- var thisobj = this;
- var memkey = 'piz'+this.tableName+JSON.stringify(objQuery).hashCode();
+  var thisobj = this;
+  var memkey;
+  try {
+    memkey = 'piz'+this.tableName+JSON.stringify(objQuery).hashCode();
+  } catch(e) {
+    return (new Error("cacheCall: Parámetros incorrectos"), undefined);
+  }
 
- sails.memcached.get(memkey, function (err, memresult) {
-	 if (err) {
-		 return callback(err, undefined);
-	 }
-	 if (typeof memresult !== 'undefined' && memresult != false) {
-		 // cache hit
-		 return callback(undefined, memresult);
-	 }
-	 // cache miss
-	 thisobj.find(objQuery).exec(function(err,result){
-		 if (err) {
-			 return callback(err, undefined);
-		 }
-		 if (!result) {
-			 return callback(new Error("No hay resultados en la consulta de "+thisobj.tableName), undefined);
-		 }
-		 sails.memcached.set(memkey, result, 84600, function(err){
-			 if (err) {
-				 return callback(err, undefined);
-			 }
-			 return callback(undefined, result);
-		 });
-	 });
- });
+  sails.memcached.get(memkey, function (err, memresult) {
+    if (!err && typeof memresult !== 'undefined' && memresult != false) {
+      // cache hit
+      return callback(undefined, memresult);
+    }
+    // ignoro las situaciones de error
+    // cache miss
+    thisobj.find(objQuery).exec(function(err,result){
+      if (err) {
+        return callback(err, undefined);
+      }
+      if (!result) {
+        return callback(undefined, result);
+      }
+      sails.memcached.set(memkey, result, 84600, function(err){
+        // ignoro las situaciones de error
+        return callback(undefined, result);
+      });
+    });
+  });
 };
 
 // función que se agrega a todos los modelos para llamar a la función objFunction
 // con parámetro objParm, tomando el resultado primero de memcached
-sails.config.models.cacheCall = function (id,objFunction,objParam,callback) {
- var thisobj = this;
- var memkey = 'piz'+this.tableName+id+'/'+JSON.stringify(objParam).hashCode();
+sails.config.models.cacheCall = function (id,objFunction,objParam,ttl,callback) {
+  var thisobj = this;
+  var memkey;
+  try {
+    memkey = 'piz'+this.tableName+id+'/'+JSON.stringify(objParam).hashCode();
+  } catch(e) {
+    return (new Error("cacheCall: Parámetros incorrectos"), undefined);
+  }
 
- sails.memcached.get(memkey, function (err, memresult) {
-	 if (err) {
-		 return callback(err, undefined);
-	 }
-	 if (typeof memresult !== 'undefined' && memresult != false) {
-		 // cache hit
-		 return callback(undefined, memresult);
-	 }
-	 // cache miss
-	 objFunction(objParam,function(err,result){
-		 if (err) {
-			 return callback(err, undefined);
-		 }
-		 if (!result) {
-			 return callback(new Error("No hay resultados en la consulta de "+JSON.stringify(objFunction)), undefined);
-		 }
-		 sails.memcached.set(memkey, result, 84600, function(err){
-			 if (err) {
-				 return callback(err, undefined);
-			 }
-			 return callback(undefined, result);
-		 });
-	 });
- });
+  sails.memcached.get(memkey, function (err, memresult) {
+    if (!err && typeof memresult !== 'undefined' && memresult != false) {
+      // cache hit
+      //console.log("cache hit: "+memkey);
+      return callback(undefined, memresult);
+    }
+    // ignoro las situaciones de error con memcached
+    //console.log("cache miss: "+memkey);
+    // cache miss
+    objFunction(objParam,function(err,result){
+      if (err) {
+        return callback(err, undefined);
+      }
+      if (!result) {
+        return callback(undefined, result);
+      }
+      sails.memcached.set(memkey, result, ttl, function(err){
+        // ignoro las situaciones de error con memcached
+        return callback(undefined, result);
+      });
+    });
+  });
 };
 
 module.exports = {
@@ -93,7 +97,7 @@ module.exports = {
 			if (err) {
 				return res.serverError(err);
 			}
-			Asignaturas.cacheCall("webces",Asignaturas.webces,{},function(err,asignaturas){
+			Asignaturas.cacheCall("webces",Asignaturas.webces,{},86400,function(err,asignaturas){
 				if (err) {
 					return res.serverError(err);
 				}
@@ -106,7 +110,7 @@ module.exports = {
 		var departamento = parseInt(req.param("departamento"));
 		var asignatura = parseInt(req.param("asignatura"));
 
-		Stock.libres(departamento,asignatura,function(err,stock){
+		Stock.cacheCall("libres",Stock.libres,{DeptoId:departamento,AsignId:asignatura},60,function(err,stock){
       if (err) {
         return res.serverError(err);
       }
@@ -125,16 +129,15 @@ module.exports = {
 		var Memcached = require('memcached');
 		sails.memcached = new Memcached(sails.config.memcached);
 
-		Stock.libres(departamento,asignatura,function(err,stock){
+		Stock.cacheCall("libres",Stock.libres,{DeptoId:departamento,AsignId:asignatura},60,function(err,stock){
 			if (err) {
 				return res.serverError(err);
 			}
-
 			Departamentos.cacheFind({DeptoId:{'<':20}},function(err,departamentos){
 				if (err) {
 					return res.serverError(err);
 				}
-				Asignaturas.cacheCall("webces",Asignaturas.webces,{},function(err,asignaturas){
+				Asignaturas.cacheCall("webces",Asignaturas.webces,{},86400,function(err,asignaturas){
 					if (err) {
 						return res.serverError(err);
 					}
@@ -175,7 +178,7 @@ module.exports = {
 												if (err) {
 													return res.serverError(err);
 												}
-												Materias.cacheCall("webces",Materias.webces,{},function(err,materias){
+												Materias.cacheCall("webces",Materias.webces,{},86400,function(err,materias){
 													if (err) {
 														return res.serverError(err);
 													}
